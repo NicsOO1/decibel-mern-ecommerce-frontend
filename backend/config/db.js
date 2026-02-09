7bbb04afcc15cf884c90c81767905c7fed16c6cf
shibzz <monsterbegin@gmail.com>
2026-02-09 16:39:10 +0530

    Secure MongoDB URI using environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGO_URI =
-  "mongodb+srv://decibel:decibel123@cluster0.tqbbv.mongodb.net/decibelDB?appName=Cluster0";
-
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database connected.");
   } catch (error) {
     console.error(error);
